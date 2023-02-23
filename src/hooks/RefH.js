63839d3f2d12ab40e32bce914c2e7bacc48a8151
raj144 <raj@logicrays.com>
2023-02-23 18:45:15 +0530

    useRef hooks

--- src/hooks/RefH.js
+++ src/hooks/RefH.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 function RefH() {
+    const inputRef = useRef(null);
+
+    const handleInput = () => {
+        console.log(inputRef);
+        inputRef.current.value = "Raju Bhai";
+        inputRef.current.focus();
+        inputRef.current.style.backgroundColor = "Red";
+    }
+    
     return (
         <>
             <Container>
                 <Row>
                     <Col>
                         <h1>RefH</h1>
+                    </Col>
+                    <Col>
+                        <input type="text" ref={inputRef} />
+                        <Button onClick={()=>handleInput()}>Click Me!</Button>
                     </Col>
                 </Row>
             </Container>
